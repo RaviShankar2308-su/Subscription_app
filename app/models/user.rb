7d93a65992d86c3a59957d6b6aa8efbe30b9916d
raviw-2022 <ravi.s@webkorps.com>
2022-09-20 03:00:56 +0530

    Devise Trackable

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:github]
+         :trackable, :omniauthable, omniauth_providers: [:github]
 
   def self.from_omniauth(access_token)
     user = User.where(email: access_token.info.email).first
     unless user
       user = User.create(
         email: access_token.info.email,
-        password: Devise.friendly_token[0,20]
+        password: Devise.friendly_token[0, 20]
       )
     end
     user.name = access_token.info.name
