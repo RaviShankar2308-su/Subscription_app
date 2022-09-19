b8754cc6fd97ba7dfcd6ed87610a7e8d6dba9078
raviw-2022 <ravi.s@webkorps.com>
2022-09-20 01:50:45 +0530

    Users Index and Show

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: 'users/sessions',
     omniauth_callbacks: 'users/omniauth_callbacks',
-    registrations: 'users/registrations'
   }
   resources :users, only: %i[index show]
   root "static_public#landing_page"
